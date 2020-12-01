25ad348a688071cc5db7b7b7f311a1cf4d3e3583
poliakov_evgenii <evg_pol@yahoo.com>
2020-12-01 15:08:00 +0300

    fcheck

--- task3/script.js
+++ task3/script.js
@@ -4,13 +4,13 @@ function makeGreen() {
     trafficLightEl.style.background = ('green');
     trafficLightEl.removeEventListener('click', makeGreen);
     trafficLightEl.addEventListener('click', makeYellow);
-};
+}
 
 function makeYellow() {
     trafficLightEl.style.background = ('yellow');
     trafficLightEl.removeEventListener('click', makeYellow);
     trafficLightEl.addEventListener('click', makeRed);
-};
+}
 
 function makeRed() {
     trafficLightEl.style.background = ('red');
